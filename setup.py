559faff6b17519dd89d51f64370a4231c2234969
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-10-26 18:45:08 +0100

    use true abi now that we switched to cython

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-import os.path
 from setuptools import setup
-
-try:
-    import wheel.bdist_wheel
-    class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
-        def finalize_options(self):
-            wheel.bdist_wheel.bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-        def get_tag(self):
-            python, abi, plat = wheel.bdist_wheel.bdist_wheel.get_tag(self)
-            python, abi = 'py2.py3', 'none'
-            return python, abi, plat
-except ImportError:
-    bdist_wheel = None
 
 setup(
     name='pygsw',
@@ -23,7 +9,6 @@ setup(
     license='GPL',
     packages=['pygsw'],
     package_data={'pygsw': ['*.so', '*.dll', '*.dylib', '*.pyd']},
-    cmdclass={'bdist_wheel': bdist_wheel},
     zip_safe=False
 )
 
