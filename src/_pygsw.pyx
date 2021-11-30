3e7ff9c2a8b7871c8f60842d77d06581784b6022
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-11-30 12:25:19 +0000

    additional checks on array extent

--- src/_pygsw.pyx
+++ src/_pygsw.pyx
@@ -22,30 +22,44 @@ def calculate_pt(lon, lat, z, t, sp):
     return pt0
 
 def sa_from_sp(const double[::1] lon not None, const double[::1] lat not None, const double[::1] p not None, const double[::1] sp not None, double[::1] out=None):
+    assert lon.shape[0] == lat.shape[0], 'Sizes of longitude (%i) and latitude (%i) do not match.' % (lon.shape[0], lat.shape[0])
+    assert lon.shape[0] == p.shape[0], 'Sizes of longitude (%i) and pressure (%i) do not match.' % (lon.shape[0], p.shape[0])
+    assert lon.shape[0] == sp.shape[0], 'Sizes of longitude (%i) and practical salinity (%i) do not match.' % (lon.shape[0], sp.shape[0])
+    assert out is None or sp.shape[0] == out.shape[0], 'Sizes of practical salinity (%i) and output (%i) do not match.' % (sp.shape[0], out.shape[0])
     if out is None:
         out = numpy.empty_like(sp)
     cgsw_sa_from_sp(out.shape[0], &sp[0], &p[0], &lon[0], &lat[0], &out[0])
     return out
 
 def pt0_from_t(const double[::1] SA not None, const double[::1] t not None, const double[::1] p not None, double[::1] out=None):
+    assert SA.shape[0] == t.shape[0], 'Sizes of absolute salinity (%i) and in-situ temperature (%i) do not match.' % (SA.shape[0], t.shape[0])
+    assert SA.shape[0] == p.shape[0], 'Sizes of absolute salinity (%i) and pressure (%i) do not match.' % (SA.shape[0], p.shape[0])
+    assert out is None or SA.shape[0] == out.shape[0], 'Sizes of absolute salinity (%i) and output (%i) do not match.' % (SA.shape[0], out.shape[0])
     if out is None:
         out = numpy.empty_like(t)
     cgsw_pt0_from_t(out.shape[0], &SA[0], &t[0], &p[0], &out[0])
     return out
 
 def ct_from_pt(const double[::1] SA not None, const double[::1] pt0 not None, double[::1] out=None):
+    assert SA.shape[0] == pt0.shape[0], 'Sizes of absolute salinity (%i) and potential temperature (%i) do not match.' % (SA.shape[0], pt0.shape[0])
+    assert out is None or SA.shape[0] == out.shape[0], 'Sizes of absolute salinity (%i) and output (%i) do not match.' % (SA.shape[0], out.shape[0])
     if out is None:
         out = numpy.empty_like(pt0)
     cgsw_ct_from_pt(out.shape[0], &SA[0], &pt0[0], &out[0])
     return out
 
 def pt_from_ct(const double[::1] SA not None, const double[::1] ct not None, double[::1] out=None):
+    assert SA.shape[0] == ct.shape[0], 'Sizes of absolute salinity (%i) and conservative temperature (%i) do not match.' % (SA.shape[0], ct.shape[0])
+    assert out is None or SA.shape[0] == out.shape[0], 'Sizes of absolute salinity (%i) and output (%i) do not match.' % (SA.shape[0], out.shape[0])
     if out is None:
         out = numpy.empty_like(ct)
     cgsw_pt_from_ct(out.shape[0], &SA[0], &ct[0], &out[0])
     return out
 
 def rho(const double[::1] SA not None, const double[::1] ct not None, const double[::1] p not None, double[::1] out=None):
+    assert SA.shape[0] == ct.shape[0], 'Sizes of absolute salinity (%i) and conservative temperature (%i) do not match.' % (SA.shape[0], ct.shape[0])
+    assert SA.shape[0] == p.shape[0], 'Sizes of absolute salinity (%i) and pressure (%i) do not match.' % (SA.shape[0], p.shape[0])
+    assert out is None or SA.shape[0] == out.shape[0], 'Sizes of absolute salinity (%i) and output (%i) do not match.' % (SA.shape[0], out.shape[0])
     if out is None:
         out = numpy.empty_like(SA)
     cgsw_rho(out.shape[0], &SA[0], &ct[0], &p[0], &out[0])
