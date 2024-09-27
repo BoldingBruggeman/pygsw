99ba81d324e18fdd09a188710457b11c1fde8c5a
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-09-27 12:50:34 +0100

    optimize linear interpolation in cgsw_nsquared_3d

--- src/wrapper.F90
+++ src/wrapper.F90
@@ -52,25 +52,23 @@ contains
       real(c_double), intent(in) :: h(nx, ny, nz), sa(nx, ny, nz), ct(nx, ny, nz), p(nx, ny, nz), lat(nx, ny)
       real(c_double), intent(inout) :: n2(nx, ny, nz-1)
 
-      real(c_double), allocatable, dimension(:,:) :: dsa, sa_mid, dct, ct_mid, dp, p_mid, h_sum_inv
+      real(c_double), allocatable, dimension(:,:) :: dsa, sa_mid, dct, ct_mid, dp, p_mid, hfrac
       real(c_double), allocatable, dimension(:,:) :: rho_mid, alpha_mid, beta_mid !, grav_mid
       real(c_double), parameter :: grav_mid = 9.81_c_double
       integer :: k
 
-      allocate (dsa(nx, ny), sa_mid(nx, ny), dct(nx, ny), ct_mid(nx, ny), dp(nx, ny), p_mid(nx, ny), h_sum_inv(nx, ny))
+      allocate (dsa(nx, ny), sa_mid(nx, ny), dct(nx, ny), ct_mid(nx, ny), dp(nx, ny), p_mid(nx, ny), step(nx, ny))
       allocate (rho_mid(nx, ny), alpha_mid(nx, ny), beta_mid(nx, ny)) !, grav_mid(nx, ny))
 
       do k=1,nz-1
-         where (mask /= 0)
-            h_sum_inv = 1._c_double / (h(:,:,k) + h(:,:,k+1))
-            !grav_mid(:,:) = h_sum_inv * (h(:,:,k+1) * gsw_grav(lat(:,:), p(:,:,k)) + h(:,:,k) * gsw_grav(lat(:,:), p(:,:,k+1)))
-            dsa(:,:) = (sa(:,:,k+1) - sa(:,:,k))
-            sa_mid(:,:) = h_sum_inv * (h(:,:,k+1) * sa(:,:,k) + h(:,:,k) * sa(:,:,k+1))
-            dct(:,:) = (ct(:,:,k+1) - ct(:,:,k))
-            ct_mid(:,:) = h_sum_inv * (h(:,:,k+1) * ct(:,:,k) + h(:,:,k) * ct(:,:,k+1))
-            dp(:,:) = (p(:,:,k+1) - p(:,:,k))
-            p_mid(:,:) = h_sum_inv * (h(:,:,k+1) * p(:,:,k) + h(:,:,k) * p(:,:,k+1))
-         end where
+         step = h(:,:,k) / (h(:,:,k) + h(:,:,k+1))
+         !grav_mid(:,:) = (h(:,:,k+1) * gsw_grav(lat(:,:), p(:,:,k)) + h(:,:,k) * gsw_grav(lat(:,:), p(:,:,k+1))) / (h(:,:,k) + h(:,:,k+1))
+         dsa(:,:) = (sa(:,:,k+1) - sa(:,:,k))
+         sa_mid(:,:) = sa(:,:,k) + step * dsa
+         dct(:,:) = (ct(:,:,k+1) - ct(:,:,k))
+         ct_mid(:,:) = ct(:,:,k) + step * dct
+         dp(:,:) = (p(:,:,k+1) - p(:,:,k))
+         p_mid(:,:) = p(:,:,k) + step * dp
          call gsw_rho_alpha_beta(sa_mid, ct_mid, p_mid, rho_mid, alpha_mid, beta_mid)
          where (mask /= 0) n2(:,:,k) = (grav_mid**2) * (rho_mid / (db2pa * dp)) * &
                          (beta_mid * dsa - alpha_mid * dct)
